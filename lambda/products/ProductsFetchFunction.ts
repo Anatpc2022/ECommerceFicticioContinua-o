e08eebafc64b9ecfa3f6724179a9de621ba2c37d
Anatpc2022 <ana.tpc2022@gmail.com>
2023-02-21 14:29:04 -0300

    Utilizando o lambda Layer na função de pesquisa de produtos

--- lambda/products/ProductsFetchFunction.ts
+++ lambda/products/ProductsFetchFunction.ts
@@ -1,5 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Context } from "vm";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from "aws-sdk";
+
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler(event: APIGatewayProxyEvent,
     context: Context): Promise<APIGatewayProxyResult> {
@@ -12,21 +19,23 @@ export async function handler(event: APIGatewayProxyEvent,
         const method = event.httpMethod
         if (event.resource === "/products") {
             if (method === 'GET') {
-                console.log('GET')
+                console.log('GET/products')
+
+                const products = await productRepository.getAllProducts()
 
                 return {
                     statusCode: 200,
-                    body: JSON.stringify({
-                        message: "GET Products - OK"
-                    })
+                    body: JSON.stringify(products)
                 }
             }
         }else if (event.resource === "/products/{id}") {
             const productId = event.pathParameters!.id as string
             console.log (`GET /products/${productId}`)
+
+            const product = await productRepository.getProductById(productId)
             return {
                 statusCode: 200,
-                body: `GET /products/${productId}`
+                body: JSON.stringify(product)
             }
         }
 
